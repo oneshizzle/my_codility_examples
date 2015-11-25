f0f0fd81247849b1790c7f0b8644f4d06a23ec5e
oneshizzile <oneshotnyc@msn.com>
2015-11-24 19:16:57 -0500

    adding Nesting
    Change-Id: I7f1888d1723875523bbc75209339ad93e0fde2b1

---
+++ scala/src/scala/Nesting.scala
@@ -0,0 +1,24 @@
+package scala
+
+object Nesting {
+
+  def solution(S: String): Int = {
+    var i: Integer = 0
+    var j: Integer = 0
+    for (ch <- S if i - j >= 0) {
+      if (ch.toString.equals("(")) {
+        i = i + 1
+      } else {
+        j = j + 1
+      }
+    }
+    if (i - j == 0) 1 else 0
+  }
+
+  def main(args: Array[String]) = {
+    //Nesting basic test no junit for now
+    assert(solution(")(") == 0, "Expected Pass")
+    assert(solution(")(") == 1, "Expected Failure")
+  }
+}
+
